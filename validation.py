6f9a911ab123cd3ada08c448370058d3ebe206db
plancher bruno <bruno.plancher@gmail.com>
2014-06-16 15:22:33 +0200

    [openbase.validation] process fully workable, have to implement the method notify_validators() to finalize it

--- validation.py
+++ validation.py
@@ -19,6 +19,7 @@
 #############################################################################
 from openbase_core import OpenbaseCore
 from osv import osv, fields
+import netsvc
 
 ## This object define a "relative validator"
 ## Linked with an openstc.service and a selection field, it permits to retrieve a user according to the services records
@@ -88,29 +89,68 @@ OpenbaseValidationItem()
 ## it can be linked to an object to add a validation system (by adding a subflow in the default wkf) 
 class OpenbaseValidation(OpenbaseCore):
     
+    ## @return: validation.item if uid corresponds to a waiting_validation_item, else return False
+    def _get_current_user_item(self, cr, uid, ids, name, args, context=None):
+        ret = {}.fromkeys(ids, False)
+        for v in self.browse(cr, uid, ids, context=context):
+            for item in v.waiting_validation_item_ids:
+                user = item.user_id
+                if user and user.id == uid:
+                    ret[v.id] = item.id
+        return ret
+    
+    _name = 'openbase.validation'
+    _columns = {
+        'name': fields.char('Origin', size=64, required=True),
+        'validation_type': fields.selection([('and', 'ET'), ('or', 'OU'), ('next', 'Ensuite')], 'Validation type'),
+        
+        'validation_item_ids':fields.many2many('openbase.validation.item', 'validation_validation_item_rel', 'validation_id', 'item_id', 'Validations'),
+        'waiting_validation_item_ids': fields.many2many('openbase.validation.item', 'validation_waiting_validation_item_rel', 'validation_id', 'item_id', 'Waiting Validations'),
+        'validation_log_ids': fields.one2many('openbase.validation.log', 'validation_id', 'Validations History'),
+        
+        'confirm_note': fields.text('Confirm note'),
+        'refuse_note': fields.text('Refuse note'),
+        
+        'current_user_item_id':fields.function(_get_current_user_item, method=True, type='many2one', relation='openbase.validation.item', string="Current user Role"),
+        'state': fields.selection([('wait','Wait a validation'), ('refused', 'Refused'), ('done', 'Validated')]),
+    }
+
+    _defaults = {
+        'validation_type': lambda *a: 'next',
+        'state': lambda *a: 'wait',
+        'name': lambda *a: '/' 
+    }
+
     ## Notify user(s) form which we are waiting a validation
     ## @param mail_template: the email.template used to send mail to the waiting validators wanted to be mailed
     def notify_validators(self, cr, uid, ids, mail_template='wait', context=None):
         #TODO
         return True
-    
+
+    ## if the uid is authorized to validate, pop the corresponding validation_item from the waiting validation, and create a log
     def apply_decision(self, cr, uid, ids, action):
         for v in self.browse(cr, uid, ids):
             #check if uid is authorized to validate
-            user_item_id = False
-            waiting_item_ids = []
-            for item in v.waiting_validation_item_ids:
-                user = item.user_id
-                if user and user.id == uid:
-                    #user authorized, create a log, and remove him from the waiting validations
-                    v.write({'waiting_validation_item_ids': [(3,item.id)],
-                             'validation_item_ids': [(3,item.id)],
-                             'validation_log_ids': [(0,0,{'note':'',
-                                                         'validation_item_id':item.id,
-                                                         'user_id': uid,
-                                                         'state': action})]
-                             })
-                    break
+            item = v.current_user_item_id
+            if item:
+                #the fields 'confirm_note', 'refuse_note' are used to supply the note written by the user
+                #we use it because wkf do not use 'context', and the validation.log is not created at the same time the *_note is written
+                note = ''
+                key_note = action + '_note' #even us can have keynotes ! ;)
+                if hasattr(v,key_note):
+                    note = v[key_note]
+                    v.write({key_note: ''})
+                #user authorized, create a log, and remove him from the waiting validations
+                v.write({'waiting_validation_item_ids': [(3,item.id)],
+                         'validation_item_ids': [(3,item.id)],
+                         'validation_log_ids': [(0,0,{'note':note,
+                                                     'validation_item_id':item.id,
+                                                     'user_id': uid,
+                                                     'state': action})]
+                         })
+            else:
+                raise osv.except_osv('401', 'This user is not authorized to confirm this validation')
+        return True
     
     ## write the first value of "waiting_validation_item_ids" according to validation_type.
     ## If type is 'next', write only the first validation.item, else, write all the validation.item
@@ -130,57 +170,53 @@ class OpenbaseValidation(OpenbaseCore):
         for v in self.browse(cr, uid, ids):
             #if validation_type is not 'next', users have already been notified when a log exists (not the better way to know if a this is the first time we pass in this activity)
             if v.validation_type == 'next' or not v.validation_log_ids:
-                self.notify_validators(cr, uid, ids, context=context)
+                self.notify_validators(cr, uid, ids)
         self.write(cr, uid, ids, {'state':'wait'})
         return True
     
-    ## if the uid is authorized to validate, pop the corresponding validation_item from the waiting validation, and create a log
     def wkf_confirm(self, cr, uid, ids):
         self.apply_decision(cr, uid, ids, 'confirm')
         return True
     
+    ## Remove other waiting validations to make the wkf done
     def wkf_do_or(self, cr, uid, ids):
         for v in self.browse(cr, uid, ids):
             v.write({'validation_item_ids':[(5,)],
                     'waiting_validation_item_ids':[(5,)]})
         return True
     
+    ## Set the next validator (if there is other waiting validations)
     def wkf_do_next(self, cr, uid, ids):
         for v in self.browse(cr, uid, ids):
             if v.validation_item_ids:
                 v.write({'waiting_validation_item_ids': [(4,v.validation_item_ids[0].id)]})
         return True
     
+    ## Remove other waiting validations and perform refusal of the validation process (send mail to all others waiting validations)
     def wkf_refused(self, cr, uid, ids):
         self.apply_decision(cr, uid, ids, 'refuse')
-        self.notify_validators(cr, uid, ids, mail_template='refused', context=context)
+        self.notify_validators(cr, uid, ids, mail_template='refused')
         self.write(cr, uid, ids, {'state':'refused',
                                   'validation_item_ids':[(5,)],
                                   'waiting_validation_item_ids':[(5,)]})
         return True
     
+    ## Send mail to all other waiting validations (to tell them they don't have to check anymore)
     def wkf_done(self, cr, uid, ids):
-        self.notify_validators(cr, uid, ids, mail_template='done', context=context)
+        self.notify_validators(cr, uid, ids, mail_template='done')
         self.write(cr, uid, ids, {'state':'done'})
         return True
     
-    _name = 'openbase.validation'
-    _columns = {
-        'name': fields.char('Origin', size=64, required=True),
-        'validation_type': fields.selection([('and', 'ET'), ('or', 'OU'), ('next', 'Ensuite')], 'Validation type'),
-        
-        'validation_item_ids':fields.many2many('openbase.validation.item', 'validation_id', 'item_id', 'validation_validation_item_rel', 'Validations'),
-        'waiting_validation_item_ids': fields.many2many('openbase.validation.item', 'validation_id', 'item_id', 'validation_waiting_validation_item_rel', 'Waiting Validations'),
-        'validation_log_ids': fields.one2many('openbase.validation.log', 'validation_id', 'Validations History'),
-        
-        'state': fields.selection([('wait','Wait a validation'), ('refused', 'Refused'), ('done', 'Validated')]),
-    }
-    
-    _defaults = {
-        'validation_type': lambda *a: 'next',
-        'state': lambda *a: 'wait',
-        'name': lambda *a: '/' 
-    }
+    def write(self, cr, uid, ids, vals, context=None):
+        state = False
+        if 'wkf_evolve' in vals:
+            state = vals.pop('wkf_evolve')
+        super(OpenbaseValidation, self).write(cr, uid, ids, vals, context=context)
+        if state:
+            wkf_service = netsvc.LocalService('workflow')
+            for id in ids:
+                wkf_service.trg_validate(uid, 'openbase.validation', id, state, cr)
+        return True
     
 OpenbaseValidation()
 
